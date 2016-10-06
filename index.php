3f82d80b9b53cdf42e181b52f7fc5b9884e9672e
Sanjay-Gunagi <sanjaysg.cse@gmail.com>
2016-10-06 19:33:34 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,11 @@
+<html>
+<head>
+<title></title>
+<link href="CSS/style.css" rel="stylesheet" type="text/css"/>
+</head>
+<body>
+<center>
+<a href="enter_details.php">See your IA results here</a><div class="loginform">   
+</center>
+</body>
+</html>
